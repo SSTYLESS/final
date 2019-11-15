7228c97dae138ae7f35ee1fbd1e06ac8c182948d
SSTYLESS <57273562+SSTYLESS@users.noreply.github.com>
2019-11-14 19:29:56 -0500

    Add files via upload

---
+++ Quiz3/Quiz3/Carwash System.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Quiz3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carbutton = new System.Windows.Forms.RadioButton();
+            this.ownerbutton = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Makebox = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // carbutton
+            // 
+            this.carbutton.AutoSize = true;
+            this.carbutton.Location = new System.Drawing.Point(95, 59);
+            this.carbutton.Name = "carbutton";
+            this.carbutton.Size = new System.Drawing.Size(41, 17);
+            this.carbutton.TabIndex = 0;
+            this.carbutton.TabStop = true;
+            this.carbutton.Text = "Car";
+            this.carbutton.UseVisualStyleBackColor = true;
+            this.carbutton.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // ownerbutton
+            // 
+            this.ownerbutton.AutoSize = true;
+            this.ownerbutton.Location = new System.Drawing.Point(399, 59);
+            this.ownerbutton.Name = "ownerbutton";
+            this.ownerbutton.Size = new System.Drawing.Size(56, 17);
+            this.ownerbutton.TabIndex = 1;
+            this.ownerbutton.TabStop = true;
+            this.ownerbutton.Text = "Owner";
+            this.ownerbutton.UseVisualStyleBackColor = true;
+            this.ownerbutton.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(41, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(41, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Year";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.Makebox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(73, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 222);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Car Registration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(41, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Make";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(103, 166);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(103, 104);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // Makebox
+            // 
+            this.Makebox.FormattingEnabled = true;
+            this.Makebox.Location = new System.Drawing.Point(103, 40);
+            this.Makebox.Name = "Makebox";
+            this.Makebox.Size = new System.Drawing.Size(121, 21);
+            this.Makebox.TabIndex = 12;
+            this.Makebox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(355, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(245, 222);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Owner";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(24, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Telephone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(24, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(24, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "First Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(153, 371);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(100, 34);
+            this.resetbutton.TabIndex = 10;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(707, 463);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.ownerbutton);
+            this.Controls.Add(this.carbutton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton carbutton;
+        private System.Windows.Forms.RadioButton ownerbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox Makebox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Quiz3/Quiz3/Carwash System.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            groupBox2.Enabled = false;
+        }
+
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = true;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Makebox.Items.Add("Honda");
+            Makebox.Items.Add("Toyota");
+            Makebox.Items.Add("Audi");
+
+
+            comboBox3.Items.Add("2000");
+            comboBox3.Items.Add("2001");
+            comboBox3.Items.Add("2002");
+            comboBox3.Items.Add("2003");
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            if (Makebox.SelectedIndex == 0)
+            {
+                comboBox2.Items.Add("Civic");
+                comboBox2.Items.Add("Accord");
+                comboBox2.Items.Add("Sport");
+            }
+            if (Makebox.SelectedIndex == 1)
+            {
+                comboBox2.Items.Add("Corolla");
+                comboBox2.Items.Add("Camry");
+                comboBox2.Items.Add("Prius");
+            }
+            if (Makebox.SelectedIndex == 2)
+            {
+                comboBox2.Items.Add("R8");
+                comboBox2.Items.Add("A4");
+                comboBox2.Items.Add("A6");
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+
+                string Make = Makebox.Text;
+                string Model = comboBox2.Text;
+                string Year = comboBox3.Text;
+                string FirstName = textBox1.Text;
+                string LastName = textBox2.Text;
+                string Telephone = textBox3.Text;
+
+                if (Make != "" && Model != "" && Year != "" && FirstName != "" && LastName != "" && Telephone != "")
+                {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.DefaultExt = "txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.Append))
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(Make + " ");
+                        sw.Write(Model + " ");
+                        sw.Write(Year + " ");
+                        sw.Write(FirstName + " ");
+                        sw.Write(LastName + " ");
+                        sw.Write(Telephone + "\n");
+                    }
+                }
+
+            }
+            
+                else
+                {
+                    MessageBox.Show("You must fill out the form");
+                }
+            }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+
+            Makebox.Text = string.Empty;
+            comboBox2.Text = string.Empty;
+            comboBox3.Text = string.Empty;
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+    }
+
+
+    }
+    
+
+ 
+    
+
